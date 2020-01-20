9e103ca3c1989ba48c630f72556c3a967e4c6a1a
Fabian Neugart <f.neugart@mytaxi.com>
2020-01-20 11:13:04 +0100

    Getting pokemon details in parallel

--- shared/src/commonMain/kotlin/com/example/dexter/repository/remote/PokemonRepository.kt
+++ shared/src/commonMain/kotlin/com/example/dexter/repository/remote/PokemonRepository.kt
@@ -3,14 +3,23 @@ package com.example.dexter.repository.remote
 import com.example.dexter.api.ClientApi
 import com.example.dexter.repository.BaseRepository
 import com.example.dexter.repository.data.Pokemon
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
 
 class PokemonRepository : BaseRepository() {
 
     suspend fun getPokemons(): List<Pokemon> {
-        return ClientApi.getPokemons().results.map {
-            // TODO: This is super slow :( Any idea @Max? -> Parallelize
-            val spriteUrl = ClientApi.getPokemonDetail(it.name).sprites.front_default
-            Pokemon(it.name, spriteUrl)
+        return ClientApi.getPokemons().results.pmap {
+            Pokemon(it.name, getPokemonDetailResponse(it.name).sprites.front_default)
         }
     }
+
+    private suspend fun getPokemonDetailResponse(name: String) =
+        ClientApi.getPokemonDetail(name)
 }
+
+suspend fun <A, B> Iterable<A>.pmap(f: suspend (A) -> B): List<B> = coroutineScope {
+    map { async { f(it) } }.awaitAll()
+}
+
