60153c5eb9237862b3eda96b378de878c66f55fd
Max Cruz <max.cruz@mytaxi.com>
2020-02-25 01:23:39 +0100

    Init iOS setup

--- ios/Dexter/AppDelegate.swift
+++ ios/Dexter/AppDelegate.swift
@@ -7,14 +7,17 @@
 //
 
 import UIKit
+import DexterShared
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
+    let appContainer = AppContainer(foreground: UI())
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        setupRootViewController(viewController: MainViewController())
         return true
     }
 
@@ -32,6 +35,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
+    func setupRootViewController(viewController: UIViewController) {
+      let navigationController = UINavigationController(rootViewController: viewController)
+      navigationController.setNavigationBarHidden(true, animated: false)
+      window = window ?? UIWindow(frame: UIScreen.main.bounds)
+      window?.rootViewController = navigationController
+      window?.makeKeyAndVisible()
+    }
 
 }
 
---
+++ ios/Dexter/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  Dexter
+//
+//  Created by Max Cruz on 25/02/2020.
+//  Copyright © 2020 Max Cruz. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
