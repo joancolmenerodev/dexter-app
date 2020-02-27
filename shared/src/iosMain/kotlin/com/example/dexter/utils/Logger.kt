c1d0f61a58f6635b6c08cc383ac70016486fe5d9
Joan <joancolmenero@gmail.com>
2020-02-27 22:07:14 +0100

    Adding logger for ios

--- shared/src/iosMain/kotlin/com/example/dexter/utils/Logger.kt
+++ shared/src/iosMain/kotlin/com/example/dexter/utils/Logger.kt
@@ -1,20 +1,27 @@
 package com.example.dexter.utils
 
+import platform.Foundation.NSLog
+
 actual class Logger {
 
     actual fun debug(message: String, formatJson: Boolean) {
+        NSLog(message)
     }
 
     actual fun error(message: String) {
+        NSLog(message)
     }
 
     actual fun warning(message: String) {
+        NSLog(message)
     }
 
     actual fun verbose(message: String) {
+        NSLog(message)
     }
 
     actual fun information(message: String) {
+        NSLog(message)
     }
 
 }
